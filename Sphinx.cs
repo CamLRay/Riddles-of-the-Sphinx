a35db10f75f070afaee7ea9a6bd0a884d5d4de98
JeremyM45 <jeremyvs1945@gmail.com>
2022-05-02 10:38:17 -0700

    add sequential looping through riddles
    Co-authored-by: Cameron Ray <crayiphone@gmail.com>

--- Sphinx.cs
+++ Sphinx.cs
@@ -5,19 +5,26 @@ class Program
 {
   static void Main()
   {
-    Dictionary<string, string> riddles = new Dictionary<string, string>() {{"Man", "What goes on four legs in the morning, two legs in the afternoon, and three legs in the evening?"}};
-    string question = "Man";
-    Console.WriteLine(riddles[question]);
-    string answer = Console.ReadLine();
-    if(question == answer)
+    Dictionary<string, string> riddles = new Dictionary<string, string>() {{"man", "What goes on four legs in the morning, two legs in the afternoon, and three legs in the evening?"}, {"a towel", "What gets wet while drying?"}};
+    string[] answerKey = {"man", "a towel"};
+    for (int i=0; i < answerKey.Length; i++)
     {
-      Console.WriteLine("Correct!");
-    }
-    else
-    {
-      Console.WriteLine("Incorrect, the Sphinx eats you.");
+      string question = answerKey[i];
+      Console.WriteLine(riddles[question]);
+      string answer = Console.ReadLine();
+      if(question == answer.ToLower())
+      {
+        Console.WriteLine("Correct!");
+
+      }
+      else
+      {
+        Console.WriteLine("Incorrect, the Sphinx eats you.");
+        break;
+      }
     }
   }
 }
 
-
+// Random rnd = new Random();
+    // int num = rnd.Next(3);
